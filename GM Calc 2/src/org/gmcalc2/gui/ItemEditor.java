02217d6a47ec7a8855161b3aa32fa53623dbae97
John Werner <mooilo.ueu@gmail.com>
2013-10-03 13:16:16 -0700

    More work on ItemEditor.

--- GM Calc 2/src/org/gmcalc2/gui/ItemEditor.java
+++ GM Calc 2/src/org/gmcalc2/gui/ItemEditor.java
@@ -7,18 +7,31 @@ import org.gmcalc2.item.TagRequirement;
 import org.haferlib.slick.gui.ArrayEditor;
 import org.haferlib.slick.gui.GUISubcontext;
 import org.haferlib.slick.gui.ListEditor;
+import org.haferlib.slick.gui.ListFrame;
+import org.haferlib.slick.gui.ScrollableListFrame;
+import org.haferlib.slick.gui.TextButton;
+import org.haferlib.slick.gui.TextDisplay;
+import org.haferlib.slick.gui.event.GUIEvent;
+import org.haferlib.slick.gui.event.GUIEventListener;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Font;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 
-public class ItemEditor extends GUISubcontext {
+public class ItemEditor extends GUISubcontext implements GUIEventListener {
+	
+	private static final String TITLE = "Edit Item";
 	
 	private final ItemDisplay itemDisplay;
 	private final Item item;
+	private final TextDisplay titleDisplay;
+	private final TextButton<?> closeButton;
+	private final ScrollableListFrame editorList;
 	private final ArrayEditor itemBaseEditor;
 	private ArrayEditor materialEditor;
 	private ListEditor prefixEditor;
 	private final Font font;
+	private int titleBorderY;
 	private final Color textColor, fieldColor, backgroundColor, searchColor;
 
 	public ItemEditor(int x, int y, int width, int height, int depth, ItemDisplay itemDisplay,
@@ -34,6 +47,30 @@ public class ItemEditor extends GUISubcontext {
 		this.backgroundColor = backgroundColor;
 		this.searchColor = searchColor;
 		
+		// Create the title display.
+		titleDisplay = new TextDisplay(x1, y1, width, font.getLineHeight(), 0, TITLE, font, textColor, TextDisplay.WIDTH_STATIC_HEIGHT_STATIC, TextDisplay.TEXT_ALIGN_CENTER);
+		titleBorderY = titleDisplay.getY() + titleDisplay.getHeight();
+		subcontext.addElement(titleDisplay);
+		
+		// Create the close button.
+		int closeButtonSize = font.getLineHeight();
+		int closeButtonX = x2 - closeButtonSize;
+		int closeButtonY = y1;
+		closeButton = new TextButton<Object>(null, closeButtonX, closeButtonY, closeButtonSize, closeButtonSize, 0,
+				fieldColor, searchColor, Input.KEY_ESCAPE, "[X]", textColor, font);
+		subcontext.addElement(closeButton);
+		
+		// Create the editor list.
+		int editorListX = x1;
+		int editorListY = titleBorderY;
+		int editorListWidth = width;
+		int editorListHeight = y2 - titleBorderY;
+		int editorListScrollBarWidth = 10;
+		int editorListXOffset = editorListScrollBarWidth;
+		editorList = new ScrollableListFrame(editorListX, editorListY, editorListWidth, editorListHeight, 0,
+				editorListScrollBarWidth, textColor, ListFrame.XALIGN_CENTER, editorListXOffset, 0);
+		subcontext.addElement(editorList);
+		
 		// Create the item base editor.
 		ItemBase itemBase = item.getItemBase();
 		
@@ -45,15 +82,13 @@ public class ItemEditor extends GUISubcontext {
 				item.getWorld().getItemBaseMap().keySet().toArray(new String[numItemBases])
 			};
 		
-		int editorX = x1;
-		int editorY = y1;
-		int editorWidth = width;
+		int editorWidth = editorList.getWidth() - editorList.getXAlignOffset() - editorList.getScrollBarWidth();
 		int editorHeight = font.getLineHeight() * 2;
-		itemBaseEditor = new ArrayEditor(editorX, editorY, editorWidth, editorHeight, 0, 0,
+		itemBaseEditor = new ArrayEditor(0, 0, editorWidth, editorHeight, 0, 0,
 				"Item Base", font, textColor, fieldColor, null, searchColor,
 				1, fieldContents, fieldMessages, fieldSearchStrings);
 		
-		subcontext.addElement(itemBaseEditor);
+		editorList.addElement(itemBaseEditor);
 		
 		createMaterialEditor();
 		createPrefixEditor();
@@ -72,12 +107,12 @@ public class ItemEditor extends GUISubcontext {
 	}
 	
 	private void destroyMaterialEditor() {
-		subcontext.removeElement(materialEditor);
+		editorList.removeElement(materialEditor);
 		materialEditor.destroy();
 	}
 	
 	private void destroyPrefixEditor() {
-		subcontext.removeElement(prefixEditor);
+		editorList.removeElement(prefixEditor);
 		prefixEditor.destroy();
 	}
 	
@@ -85,9 +120,7 @@ public class ItemEditor extends GUISubcontext {
 		Component[] materials = item.getMaterials();
 		if (materials.length <= 0)
 			return;
-		int editorX = x1;
-		int editorY = itemBaseEditor.getY() + itemBaseEditor.getHeight();
-		int editorWidth = width;
+		int editorWidth = editorList.getWidth() - editorList.getXAlignOffset() - editorList.getScrollBarWidth();
 		int editorYSpacing = 2;
 		int editorHeight = font.getLineHeight() * (1 + materials.length) + editorYSpacing * (materials.length - 1);
 		
@@ -109,24 +142,18 @@ public class ItemEditor extends GUISubcontext {
 			}
 		}
 		
-		materialEditor = new ArrayEditor(editorX, editorY, editorWidth, editorHeight, 0, editorYSpacing,
+		materialEditor = new ArrayEditor(0, 0, editorWidth, editorHeight, 0, editorYSpacing,
 				"Materials", font, textColor, fieldColor, null, searchColor,
 				materials.length, fieldContents, fieldMessages, fieldSearchStrings);
 		
-		subcontext.addElement(materialEditor);
+		editorList.addElement(materialEditor);
 	}
 	
 	private void createPrefixEditor() {
 		Component[] prefixes = item.getPrefixes();
-		int editorX = x1;
-		int editorY;
-		if (materialEditor == null)
-			editorY = itemBaseEditor.getY() + itemBaseEditor.getHeight();
-		else
-			editorY = materialEditor.getY() + materialEditor.getHeight();
-		int editorWidth = width;
+		int editorWidth = editorList.getWidth() - editorList.getXAlignOffset() - editorList.getScrollBarWidth();
 		int editorYSpacing = 2;
-		int editorHeight = y2 - editorY;
+		int editorHeight = Integer.MAX_VALUE;
 		
 		String fieldMessage = "Type a prefix filepath here.";
 		
@@ -137,7 +164,7 @@ public class ItemEditor extends GUISubcontext {
 			fieldSearchStrings[i] = possiblePrefixes[i].getFilePath();
 		}
 		
-		prefixEditor = new ListEditor(editorX, editorY, editorWidth, editorHeight, 0, editorYSpacing,
+		prefixEditor = new ListEditor(0, 0, editorWidth, editorHeight, 0, editorYSpacing,
 				"Prefixes", font, textColor, fieldColor, null, searchColor);
 		prefixEditor.setDefaultFieldMessage(fieldMessage);
 		prefixEditor.setDefaultFieldSearchStrings(fieldSearchStrings);
@@ -145,7 +172,32 @@ public class ItemEditor extends GUISubcontext {
 			prefixEditor.addField(prefixes[i].getFilePath(), fieldMessage, fieldSearchStrings);
 		}
 		
-		subcontext.addElement(prefixEditor);
+		editorList.addElement(prefixEditor);
+	}
+	
+	@Override
+	public void setY(int y) {
+		super.setY(y);
+		titleBorderY = y1 + font.getLineHeight();
+	}
+	
+	@Override
+	public void setWidth(int w) {
+		super.setWidth(w);
+		closeButton.setX(x2 - closeButton.getWidth());
+		titleDisplay.setWidth(width);
+		editorList.setWidth(width);
+		int edWidth = editorList.getWidth() - editorList.getScrollBarWidth() - editorList.getXAlignOffset();
+		itemBaseEditor.setWidth(edWidth);
+		if (materialEditor != null)
+			materialEditor.setWidth(edWidth);
+		prefixEditor.setWidth(edWidth);
+	}
+	
+	@Override
+	public void setHeight(int h) {
+		super.setHeight(h);
+		editorList.setHeight(y2 - titleBorderY);
 	}
 
 	@Override
@@ -157,9 +209,18 @@ public class ItemEditor extends GUISubcontext {
 		// Draw the border.
 		g.setColor(textColor);
 		g.drawRect(x1, y1, width, height);
+		g.drawLine(x1, titleBorderY, x2, titleBorderY);
 		
 		// Draw the subcontext.
 		renderSubcontext(g, x1, y1, x2, y2);
+	}
+
+	@Override
+	public void guiEvent(GUIEvent<?> event) {
+		// If we receive an event from the close button, set dead to true.
+		if (event.getGenerator() == closeButton) {
+			dead = true;
+		}
 	}
 
 }
